58d4acfe7f8fce350b717d7e8caafc5e3578e902
Maria <maria_ahokas@yahoo.com>
2021-07-31 12:19:03 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+// Import React library
+
+import React from "react";
+
+import ProductList from "./components/ProductList";
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello,</p>
+
+      <p>
+        This is a sample application to demonstrate the use of{" "}
+        <strong>
+          <em>TediousJS within Electron/React App</em>
+        </strong>
+      </p>
+
+      <hr />
+
+      <ProductList />
+    </div>
+  );
+};
+
+// Export the main component
+
+export default App;
---
+++ src/app/components/ProductList.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { ipcRenderer as ipc } from "electron";
+
+interface Product {
+  Name: string;
+  ProductNumber: string;
+}
+let productsFromDB: Product[] = [];
+
+const ProductList = () => {
+  const [show, showProducts] = useState(false);
+  console.log("DB", productsFromDB);
+  const getProducts = () => {
+    ipc.invoke("getproducts").then((products) => {
+      console.log("product", products);
+      productsFromDB = products;
+      showProducts(true);
+    });
+  };
+
+  if (!productsFromDB.length) {
+    getProducts();
+  }
+
+  return (
+    show && (
+      <>
+        <h1>Products:</h1>
+        {productsFromDB.map((p, index) => (
+          <div key={index}>
+            {p.Name} - {p.ProductNumber}
+          </div>
+        ))}
+      </>
+    )
+  );
+};
+
+export default ProductList;
